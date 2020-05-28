214b8bf8d9efeb9b1332237a63706d249325fa23
coding-fantastic <alexnzioka@protonmail.com>
2020-05-28 22:53:52 +0300

    URL routing and requests

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,5 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request,*args, **kwargs):
+    print(request.user)
     return HttpResponse("<h1> Hello World</h1>") # string of html code
+
+def contact_view(request ,*args, **kwargs):
+    print(request.user)
+    return HttpResponse("<h1> Contact Page</h1>") # string of html code
+
+def about_view(request, *args, **kwargs):
+    print(request.user)
+    return HttpResponse("<h1>About Page</h1>")
+
+def social_view(request , *args, **kwargs):
+    print(request.user)
+    return HttpResponse("<h1>Social Page</h1>")
