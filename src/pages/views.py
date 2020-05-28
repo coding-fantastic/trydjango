ac63575062f3021681d08cdf50c853f327ade547
coding-fantastic <alexnzioka@protonmail.com>
2020-05-28 23:08:01 +0300

    django templating

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,16 +4,20 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request,*args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1> Hello World</h1>") # string of html code
+    # return HttpResponse("<h1> Hello World</h1>") # string of html code
+    return render(request, "home.html",{})
 
 def contact_view(request ,*args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1> Contact Page</h1>") # string of html code
+    # return HttpResponse("<h1> Contact Page</h1>") # string of html code
+    return render(request, "content.html",{})
 
 def about_view(request, *args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1>About Page</h1>")
+    # return HttpResponse("<h1>About Page</h1>")
+    return render(request, "about.html",{})
 
 def social_view(request , *args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1>Social Page</h1>")
+    # return HttpResponse("<h1>Social Page</h1>")
+    return render(request, "social.html",{})
