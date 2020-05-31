47e750cdb8d79eeb10efb94985ebe98946f616ad
coding-fantastic <alexnzioka@protonmail.com>
2020-05-31 18:45:13 +0300

    pure django form with form validation

--- src/products/forms.py
+++ src/products/forms.py
@@ -9,3 +9,7 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+class RawProductForm(forms.Form):
+    title       = forms.CharField()
+    description = forms.CharField()
+    price       = forms.DecimalField()
